3aad41a971829719cad124df6396db15c032a2cc
Maksym <blooreyn@gmail.com>
2020-05-25 19:30:38 +0300

    Add job schedule for show statistic

--- src/main/java/org/softkit/test/hryhoriev/dao/QuoteDao.java
+++ src/main/java/org/softkit/test/hryhoriev/dao/QuoteDao.java
@@ -1,6 +1,7 @@
 package org.softkit.test.hryhoriev.dao;
 
 import java.util.List;
+
 import org.softkit.test.hryhoriev.entity.QuoteDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,7 +16,8 @@ public interface QuoteDao extends JpaRepository<QuoteDto, Long> {
 
     @Query(value = "SELECT *" +
             " FROM t_quote" +
-            " ORDER BY latest_price, company_name ASC LIMIT ?1",
+            " WHERE latest_price IS NOT NULL" +
+            " ORDER BY latest_price DESC, company_name ASC LIMIT ?1",
             nativeQuery = true)
     List<QuoteDto> findLimitOrderByQuoteValue(int limit);
 
---
+++ src/main/java/org/softkit/test/hryhoriev/jobs/ShowStatisticJob.java
@@ -0,0 +1,67 @@
+package org.softkit.test.hryhoriev.jobs;
+
+import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.asciithemes.a7.A7_Grids;
+import org.softkit.test.hryhoriev.service.IEXCloudService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@EnableScheduling
+public class ShowStatisticJob {
+    private static final int LIMIT = 5;
+    public static final String COMPANY_NAME = "Company name";
+    public static final String LATEST_PRICE = "Latest price";
+    public static final String PERCENT_CHANGE = "Percent change";
+
+
+    @Autowired
+    private IEXCloudService iexCloudService;
+
+    @Scheduled(cron = "${show.statistic.cron}")
+    @Transactional(isolation = Isolation.READ_COMMITTED)
+    private void showData() {
+        AsciiTable gratesValueTable = new AsciiTable();
+        gratesValueTable.getContext().setGrid(A7_Grids.minusBarPlusEquals());
+        gratesValueTable.getContext().setWidth(90);
+
+        gratesValueTable.addRule();
+        gratesValueTable.addRow(COMPANY_NAME, LATEST_PRICE);
+        gratesValueTable.addRule();
+        iexCloudService.getQuotesByLargestValue(LIMIT)
+                .forEach(quote -> {
+                    gratesValueTable.addRule();
+                    gratesValueTable.addRow(quote.getCompanyName(), quote.getLatestPrice());
+                });
+        gratesValueTable.addRule();
+
+        System.out.println("The top 5 company by highest stocks value as of " + LocalDate.now());
+        System.out.println(gratesValueTable.render());
+        System.out.println();
+
+        AsciiTable gratesChangePercentTable = new AsciiTable();
+        gratesChangePercentTable.getContext().setGrid(A7_Grids.minusBarPlusEquals());
+        gratesChangePercentTable.getContext().setWidth(90);
+
+        gratesChangePercentTable.addRule();
+        gratesChangePercentTable.addRow(COMPANY_NAME, PERCENT_CHANGE);
+        gratesChangePercentTable.addRule();
+        iexCloudService.getQuotesByLargestPercentChanges(LIMIT)
+                .forEach(quote -> {
+                    gratesChangePercentTable.addRule();
+                    gratesChangePercentTable.addRow(quote.getCompanyName(), quote.getChangePercent());
+                });
+        gratesChangePercentTable.addRule();
+
+        System.out.println("The top 5 company by highest stocks value as of " + LocalDate.now());
+        System.out.println(gratesChangePercentTable.render());
+        System.out.println();
+
+    }
+}
